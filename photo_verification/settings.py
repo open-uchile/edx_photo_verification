ebc354907ed71d71239cf6ad7598cb2af2bdc439
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-01-23 19:55:49 +0000

    Adding mysql support

--- photo_verification/settings.py
+++ photo_verification/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 from os import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -78,10 +79,21 @@ WSGI_APPLICATION = "photo_verification.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": environ.get("MYSQL_DATABASE"),
+        "USER": environ.get("MYSQL_USER"),
+        "PASSWORD": environ.get("MYSQL_PASSWORD"),
+        "HOST": environ.get("MYSQL_HOST"),
+    },
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
